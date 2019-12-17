0804bf41a24bb4342a8efd4d343d01e2830cf738
Quang-Hieu Pham <pqhieu1192@gmail.com>
2019-12-17 13:12:41 +0800

    Temporarily remove XConv

--- torch3d/models/__init__.py
+++ torch3d/models/__init__.py
@@ -1,12 +1,10 @@
 from .pointnet import PointNet
 from .pointnet2 import PointNetSSG
-from .pointcnn import PointCNN
 from .dgcnn import DGCNN
 
 
 __all__ = [
     "PointNet",
     "PointNetSSG",
-    "PointCNN",
     "DGCNN",
 ]
--- torch3d/nn/conv.py
+++ torch3d/nn/conv.py
@@ -2,7 +2,6 @@ import torch
 import torch.nn as nn
 from collections.abc import Iterable
 from torch3d.nn import functional as F
-from math import ceil
 
 
 class EdgeConv(nn.Sequential):
@@ -79,74 +78,3 @@ class SetConv(nn.Module):
         x = self.mlp(x)
         x = self.maxpool(x).squeeze(3)
         return q, x
-
-
-class XConv(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, dilation=1, bias=True):
-        super(XConv, self).__init__()
-        self.in_channels = in_channels - 3
-        self.out_channels = out_channels
-        self.mid_channels = out_channels // 4
-        self.kernel_size = kernel_size
-        self.dilation = dilation
-        self.bias = bias
-        self.mlp = nn.Sequential(
-            nn.Conv2d(3, self.mid_channels, 1, bias=self.bias),
-            nn.BatchNorm2d(self.mid_channels),
-            nn.ReLU(True),
-            nn.Conv2d(self.mid_channels, self.mid_channels, 1, bias=self.bias),
-            nn.BatchNorm2d(self.mid_channels),
-            nn.ReLU(True),
-        )
-        self.stn = nn.Sequential(
-            nn.Conv2d(3, self.kernel_size ** 2, [1, self.kernel_size], bias=self.bias),
-            nn.BatchNorm2d(self.kernel_size ** 2),
-            nn.ReLU(True),
-            # nn.Conv2d(self.kernel_size ** 2, self.kernel_size ** 2, 1, bias=self.bias),
-            # nn.BatchNorm2d(self.kernel_size ** 2),
-            # nn.ReLU(True),
-            # nn.Conv2d(self.kernel_size ** 2, self.kernel_size ** 2, 1, bias=self.bias),
-            # nn.BatchNorm2d(self.kernel_size ** 2),
-        )
-        in_channels = self.in_channels + self.mid_channels
-        dm = int(ceil(out_channels / in_channels))
-        self.conv = nn.Sequential(
-            nn.Conv2d(
-                in_channels,
-                in_channels * dm,
-                [1, self.kernel_size],
-                groups=in_channels,
-            ),
-            nn.Conv2d(in_channels * dm, self.out_channels, 1, bias=self.bias),
-            nn.BatchNorm2d(self.out_channels),
-            nn.ReLU(True),
-        )
-
-    def forward(self, p, q, x=None):
-        batch_size = p.shape[0]
-        _, index = F.knn(p, q, self.kernel_size * self.dilation)
-        index = index[..., :: self.dilation]
-        index = index.view(batch_size, -1).unsqueeze(1)
-        p_hat = torch.gather(p, 2, index.expand(-1, 3, -1))
-        p_hat = p_hat.view(batch_size, 3, -1, self.kernel_size)
-        p_hat = p_hat - q.unsqueeze(3).expand(-1, -1, -1, self.kernel_size)
-        x_hat = self.mlp(p_hat)
-        if x is not None:
-            x = torch.gather(x, 2, index.expand(-1, self.in_channels, -1))
-            x = x.view(batch_size, self.in_channels, -1, self.kernel_size)
-            x_hat = torch.cat([x_hat, x], dim=1)
-        print(p_hat.shape)
-        T = self.stn(p_hat)
-        print(T.shape)
-        T = T.view(batch_size, self.kernel_size, self.kernel_size, -1)
-        T = T.permute(0, 3, 1, 2).unsqueeze(1)
-        x = torch.matmul(T, x_hat.unsqueeze(4)).squeeze(4)
-        x = self.conv(x).squeeze(3)
-        return q, x
-
-
-if __name__ == "__main__":
-    p = torch.rand(1, 3, 1024)
-    q = torch.rand(1, 3, 512)
-    m = XConv(3, 48, 8, dilation=1, bias=False)
-    m(p, q)
