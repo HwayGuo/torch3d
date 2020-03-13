64268df81679fb80b282f2747ef0264af2ad3584
Quang-Hieu Pham <quang-hieupham@lyft.com>
2020-03-13 16:06:02 -0700

    Rectify & visualize point cloud

--- torch3d/datasets/kitti.py
+++ torch3d/datasets/kitti.py
@@ -5,20 +5,24 @@ import numpy as np
 import torch.utils.data as data
 from PIL import Image
 
+import pyvista as pv
+
 
 class KITTIDetection(data.Dataset):
-    """
-    The `KITTI Detection <http://www.cvlibs.net/datasets/kitti/>`_ dataset.
+    """The KITTI Detection dataset.
 
     Args:
       root (string): Root directory of the KITTI dataset.
-      split (string, optional): The dataset subset which can be either ``"train"``,
-        ``"val"``, or ``"test"``. Default: ``"train"``.
-      transforms (callable, optional): A function/transform that takes input sample
-        and its target as entry and return a transformed version. Default: ``None``.
+      split (string, optional): The dataset subset which can be either
+        "train", "val", or "test". Default: ``"train"``.
+      transforms (callable, optional): A function/transform that takes
+        input sample and its target as entry and return a transformed
+        version. Default: ``None``.
+      rectified (bool, optional): If True, return the LiDAR point cloud
+        in the camera rectified coordinate system. Default: ``True``.
     """  # noqa
 
-    def __init__(self, root, split="train", transforms=None):
+    def __init__(self, root, split="train", transforms=None, rectified=True):
         super(KITTIDetection, self).__init__()
         self.root = root
         if split in ["train", "val"]:
@@ -26,6 +30,7 @@ class KITTIDetection(data.Dataset):
         else:
             self.split = "testing"
         self.transforms = transforms
+        self.rectified = rectified
 
         self.image_path = os.path.join(root, self.split, "image_2")
         self.lidar_path = os.path.join(root, self.split, "velodyne")
@@ -42,6 +47,14 @@ class KITTIDetection(data.Dataset):
                 if re.match("^[0-9]{6}.bin$", x)
             ]
         )
+
+    @staticmethod
+    def lidar_to_rect(lidar, calib):
+        n = lidar.shape[0]
+        xyzw = np.c_[lidar[:, 0:3], np.ones(n)]
+        xyz = np.dot(xyzw, np.dot(calib["velo_to_cam"].T, calib["R0"].T))
+        intensity = lidar[:, 3]
+        return np.c_[xyz, intensity]
 
     def _check_integrity(self):
         return (
@@ -62,6 +75,9 @@ class KITTIDetection(data.Dataset):
         target = None
         if self.split == "training":
             target = self._get_label(frameid)
+
+        if self.rectified:
+            lidar = self.lidar_to_rect(lidar, calib)
 
         inputs = {"image": image, "lidar": lidar, "calib": calib}
         if self.transforms is not None:
@@ -112,7 +128,7 @@ class KITTIDetection(data.Dataset):
 
     def parse_kitti_label(self, filename):
         annotations = {
-            "name": [],
+            "class": [],
             "truncated": [],
             "occluded": [],
             "alpha": [],
@@ -123,7 +139,7 @@ class KITTIDetection(data.Dataset):
         }
         with open(filename, "r") as fp:
             lines = [line.strip().split(" ") for line in fp.readlines()]
-        annotations["name"] = np.array([x[0] for x in lines])
+        annotations["class"] = np.array([x[0] for x in lines])
         annotations["truncated"] = np.array([float(x[1]) for x in lines])
         annotations["occluded"] = np.array([int(x[2]) for x in lines])
         annotations["alpha"] = np.array([float(x[3]) for x in lines])
@@ -136,3 +152,46 @@ class KITTIDetection(data.Dataset):
         annotations["center"] = annotations["center"].reshape(-1, 3)
         annotations["yaw"] = annotations["yaw"].reshape(-1)
         return annotations
+
+
+if __name__ == "__main__":
+    dataset = KITTIDetection("data/KITTI", rectified=True)
+
+    pv.set_plot_theme("night")
+    for inputs, target in dataset:
+        lidar = inputs["lidar"]
+        plt = pv.Plotter()
+        mesh = pv.PolyData(lidar[:, 0:3])
+        mesh["intensity"] = lidar[:, 3]
+        plt.add_mesh(mesh, render_points_as_spheres=True, cmap="viridis")
+        plt.add_axes_at_origin()
+
+        def roty(angle):
+            c = np.cos(angle)
+            s = np.sin(angle)
+            return np.array([[c, 0, s], [0, 1, 0], [-s, 0, c]])
+
+        for i in range(len(target["class"])):
+            if target["class"][i] == "DontCare":
+                continue
+
+            print(target["class"][i])
+            R = roty(target["yaw"][i])
+            h, w, l = target["size"][i, 0:3]
+            x = [l / 2, l / 2, -l / 2, -l / 2, l / 2, l / 2, -l / 2, -l / 2]
+            y = [0, 0, 0, 0, -h, -h, -h, -h]
+            z = [w / 2, -w / 2, -w / 2, w / 2, w / 2, -w / 2, -w / 2, w / 2]
+            obb = np.vstack([x, y, z])
+            obb = np.dot(R, np.vstack([x, y, z]))
+            obb = obb.T + target["center"][i]
+
+            bounds = np.c_[np.amin(obb, axis=0), np.amax(obb, axis=0)]
+            bounds = np.ravel(bounds, order="C")
+            box = pv.Box(bounds)
+            plt.add_mesh(box, style="wireframe", line_width=8.0)
+
+        plt.enable_eye_dome_lighting()
+        plt.show()
+        plt.close()
+
+        break
